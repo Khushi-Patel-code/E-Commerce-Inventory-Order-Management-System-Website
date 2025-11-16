68f2cfd3b7ee440611c29a72905a6f079c38dcea
Khushi-Patel-code <khuship2708@gmail.com>
2025-11-16 16:22:40 -0500

    External API

--- routes/external.js
+++ routes/external.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const externalController = require("../controllers/externalController");
 
-router.get("/fetch-external-products", externalController.fetchAndSaveExternalProducts);
+router.post("/fetch-external-products", externalController.fetchAndSaveExternalProducts);
 
 module.exports = router;
